ceed46d9f9663fa8cc5f9f2e3521db169959a245
Nickolay Petroff <geniusbg@gmail.com>
2020-08-13 13:14:27 +0300

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(5080); //the server object listens on port 8080
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://blood:bl00d@localhost:27017/blood', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const User = mongoose.model('User', { name: String, birthday: Number, bloodGroup: String });
+
+const Gosho = new User({ name: 'Gosho', birthday: 1983, bloodGroup: "A+"});
+Gosho.save().then(() => console.log(Gosho));
+
+User.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+})
+
